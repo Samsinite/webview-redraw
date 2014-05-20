2203c8eece8eb7fe099acc86e48d146f1ce30651
Sam Clopton <samsinite@gmail.com>
2014-05-20 11:13:24 -0700

    ...

--- src/android/Redraw.java
+++ src/android/Redraw.java
@@ -5,6 +5,8 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import java.lang.Exception;
 
 public class Redraw extends CordovaPlugin {
 	private CordovaWebView _webView;
@@ -20,12 +22,17 @@ public class Redraw extends CordovaPlugin {
 	@Override
 	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
 		if (action.equals("redraw")) {
+			try {
 			_cordova.getActivity().runOnUiThread(new Runnable() {
 				public void run() {
 					_webView.invalidate();
 					callbackContext.success("Invalidate was called!");
 				}
 			});
+			}
+			catch (Exception e) {
+				callbackContext.error(ExceptionUtils.getStackTrace(e));
+			}
 		}
 		else {
 			return false;
